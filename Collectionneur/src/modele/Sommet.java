3183e7b946315538959ea72e4dd9f551ba107530
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-08 19:38:11 +0200

    ajout modele Sommet

--- Collectionneur/src/modele/Sommet.java
+++ Collectionneur/src/modele/Sommet.java
@@ -3,9 +3,32 @@ package modele;
 public class Sommet {
 	
 	protected int id;
-	
+	protected String nom;
+	protected int altitude;
+	protected int idChaineDeMontagne; // a remplacer par reference vers l'objet de flotte
 	public int getId() {
 		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getAltitude() {
+		return altitude;
+	}
+	public void setAltitude(int altitude) {
+		this.altitude = altitude;
+	}
+	public int getIdChaineDeMontagne() {
+		return idChaineDeMontagne;
+	}
+	public void setIdChaineDeMontagne(int idChaineDeMontagne) {
+		this.idChaineDeMontagne = idChaineDeMontagne;
 	}
 
 }
