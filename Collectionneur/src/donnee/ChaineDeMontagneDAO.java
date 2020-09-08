c8f073aaf7802b954f0534f6d2ea7e4013e4f26d
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-08 19:39:10 +0200

    ajout fonction detaillerChaineDeMontagne() dans ChaineDeMontagneDAO

--- Collectionneur/src/donnee/ChaineDeMontagneDAO.java
+++ Collectionneur/src/donnee/ChaineDeMontagneDAO.java
@@ -14,21 +14,23 @@ import modele.ChaineDeMontagne;
 
 public class ChaineDeMontagneDAO {
 
-	public List<ChaineDeMontagne> listerChainesDeMontagnes()
-	{
+	public List<ChaineDeMontagne> listerChainesDeMontagnes() {
 		Connection connection = BaseDeDonnees.getInstance().getConnection();
 		
 		List<ChaineDeMontagne> listeChainesDeMontagnes =  new ArrayList<ChaineDeMontagne>();			
 		Statement requeteListeChainesDeMontagnes;
+		
 		try {
 			requeteListeChainesDeMontagnes = connection.createStatement();
 			ResultSet curseurListeChainesDeMontagnes = requeteListeChainesDeMontagnes.executeQuery("SELECT * from chainedemontagne");
-			while(curseurListeChainesDeMontagnes.next())
-			{
+			while (curseurListeChainesDeMontagnes.next()) {
+				
 				int id = curseurListeChainesDeMontagnes.getInt("id");
 				String nom = curseurListeChainesDeMontagnes.getString("nom");
+				
 				ChaineDeMontagne collection = new ChaineDeMontagne();
 				collection.setId(id);
+				
 				collection.setNom(nom);
 				listeChainesDeMontagnes.add(collection);
 			}
@@ -39,21 +41,20 @@ public class ChaineDeMontagneDAO {
 		return listeChainesDeMontagnes;
 	}
 
-	public ChaineDeMontagne listerSommetsParChaineDeMontagne(int numero)
-	{
+	public ChaineDeMontagne detaillerChaineDeMontagne(int id) {
 		Connection connection = BaseDeDonnees.getInstance().getConnection();
 		
 		ChaineDeMontagne chaineDeMontagne =  new ChaineDeMontagne();			
 		PreparedStatement requeteChaineDeMontagne;
 			try {
 				requeteChaineDeMontagne = connection.prepareStatement("SELECT * from chainedemontagne WHERE id = ?");
-				requeteChaineDeMontagne.setInt(1, numero);
+				requeteChaineDeMontagne.setInt(1, id);
 				
-				ResultSet curseurCollection = requeteChaineDeMontagne.executeQuery();
-				curseurCollection.next();
-				int id = curseurCollection.getInt("id");
-				String nom = curseurCollection.getString("nom");
-				String description = curseurCollection.getString("description");
+				ResultSet curseur = requeteChaineDeMontagne.executeQuery();
+				curseur.next();
+				id = curseur.getInt("id");
+				String nom = curseur.getString("nom");
+				String description = curseur.getString("description");
 				chaineDeMontagne.setId(id);
 				chaineDeMontagne.setNom(nom);
 				chaineDeMontagne.setDescription(description);
