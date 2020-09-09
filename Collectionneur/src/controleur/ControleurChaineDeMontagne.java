050d6fb95a22290673aaf8cf06aff20f9ade86e9
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-09 23:05:38 +0200

    ajout bouton ajouter à VueChainesDeMontagnes

--- Collectionneur/src/controleur/ControleurChaineDeMontagne.java
+++ Collectionneur/src/controleur/ControleurChaineDeMontagne.java
@@ -2,10 +2,11 @@ package controleur;
 
 import com.sun.media.jfxmedia.logging.Logger;
 
+import vue.Navigateur;
+
 public class ControleurChaineDeMontagne extends Controleur{
 
-	public ControleurChaineDeMontagne()
-	{
+	public ControleurChaineDeMontagne() {
 		Logger.logMsg(Logger.INFO, "new ControleurChaineDeMontagne()");
 	}
 
